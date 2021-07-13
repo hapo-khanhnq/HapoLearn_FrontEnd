0c4ae1ababef1bfb4c24b8f47e9a0324767534b0
khanhnq9820 <khanh.nq@haposoft.com>
2021-07-13 11:08:07 +0700

    complete header_section + responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
     $('.fa-bars').click(function() {
         $(this).toggleClass('fa-times');
-        $('.mobile-nav').toggleClass('nav-toggle');
+        //$('.mobile-nav').toggleClass('nav-toggle');
+    });
+    $(window).on('load scroll', function() {
+        if ($(window).scrollTop() > 10) {
+            $('header').addClass('header-active');
+        } else {
+            $('header').removeClass('header-active');
+        }
     });
     /*$(window).on('load scroll', function() {
         $('.fa-bars').removeClass('fa-times');
         $('.mobile-nav').removeClass('nav-toggle');
     });*/
+
 });
